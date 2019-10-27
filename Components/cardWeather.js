d69b5e63af5bd9979229c69bcfe6f8ef40cf8da0
shelly ambar <ambarshelly@gmail.com>
2019-10-27 12:43:55 +0200

    change 'Date' constructor

--- Components/cardWeather.js
+++ Components/cardWeather.js
@@ -15,25 +15,25 @@ import {
 export default class CardWeather extends Component {
   constructor(props) {
     super(props);
+
+    const newDate = new Date(this.props.Date).toUTCString();
+    const { Link, Temperature, WeatherText, Icon } = this.props;
     this.state = {
-      expanded: false
-    };
-  }
-
-  handleExpandClick = () => {
-    this.setState({
-      expanded: !this.state.expanded
-    });
-  };
-
-  handleExpandClickFavorite = () => {
-    const { Date, Link, Temperature, WeatherText, Icon } = this.props;
-    var weatherObject = {
-      Date: Date,
+      Date: newDate,
       Link: Link,
       Temperature: Temperature,
       WeatherText: WeatherText,
       Icon: Icon
+    };
+  }
+
+  handleExpandClickFavorite = () => {
+    var weatherObject = {
+      Date: this.state.Date,
+      Link: this.state.Link,
+      Temperature: this.state.Temperature,
+      WeatherText: this.state.WeatherText,
+      Icon: this.state.Icon
     };
     var favorites = [];
     favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
@@ -42,23 +42,26 @@ export default class CardWeather extends Component {
   };
 
   render() {
-    const { Date, i, Link, Temperature, WeatherText, Icon } = this.props;
     return (
       <div>
         <MDBCol>
           <MDBCard style={{ width: "20rem", height: "22rem" }}>
             <MDBCardImage
               style={{ height: 100, width: 200 }}
-              src={`https://www.accuweather.com/images/weathericons/${Icon}.svg`}
+              src={`https://www.accuweather.com/images/weathericons/${this.state.Icon}.svg`}
               waves
             />
             <MDBCardBody>
-              <MDBCardTitle>{new Date(Date).toUTCString()}</MDBCardTitle>
+              <MDBCardTitle>{this.state.Date}</MDBCardTitle>
               <MDBCardText>
-                {WeatherText + " Temperature:  " + Temperature}
+                {this.state.WeatherText +
+                  " Temperature:  " +
+                  this.state.Temperature}
               </MDBCardText>
 
-              <MDBBtn href={Link}>navigate to website</MDBBtn>
+              <MDBBtn style={{ marginButtom: 10 }} href={this.state.Link}>
+                navigate to website
+              </MDBBtn>
               <MDBBtn onClick={this.handleExpandClickFavorite}>
                 Add To Favorites
               </MDBBtn>
